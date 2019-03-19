323e412ee2e79ca8cd340b85639090cf240be8ac
Dave Dunkin <dave.dunkin@logos.com>
2019-03-18 20:48:09 -0700

    Use Cake.Tool, simplify build.sh.

--- build.sh
+++ build.sh
@@ -1,63 +1,20 @@
 #!/usr/bin/env bash
-##########################################################################
-# This is the Cake bootstrapper script for Linux and OS X.
-# This file was downloaded from https://github.com/cake-build/resources
-# Feel free to change this file to fit your needs.
-##########################################################################
 
-# Define directories.
+set -e
+
 SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 TOOLS_DIR=$SCRIPT_DIR/cake
-NUGET_EXE=$TOOLS_DIR/nuget.exe
-NUGET_URL=https://dist.nuget.org/win-x86-commandline/latest/nuget.exe
-CAKE_VERSION=0.30.0
-CAKE_EXE=$TOOLS_DIR/Cake.$CAKE_VERSION/Cake.exe
+CAKE_VERSION=0.32.1
+CAKE_TOOL=$TOOLS_DIR/dotnet-cake
 
-# Define default arguments.
-SCRIPT_ARGUMENTS=$@
-
-# Make sure the tools folder exist.
-if [ ! -d "$TOOLS_DIR" ]; then
-  mkdir "$TOOLS_DIR"
+if [[ -f "$CAKE_TOOL" && "$("$CAKE_TOOL" --version)" != "$CAKE_VERSION" ]]; then
+  dotnet tool uninstall Cake.Tool --tool-path "$TOOLS_DIR"
 fi
 
-###########################################################################
-# INSTALL NUGET
-###########################################################################
-
-# Download NuGet if it does not exist.
-if [ ! -f "$NUGET_EXE" ]; then
-    echo "Downloading NuGet..."
-    curl -Lsfo "$NUGET_EXE" $NUGET_URL
-    if [ $? -ne 0 ]; then
-        echo "An error occurred while downloading nuget.exe."
-        exit 1
-    fi
+if [[ ! -f "$CAKE_TOOL" ]]; then
+  dotnet tool install Cake.Tool --version $CAKE_VERSION --tool-path "$TOOLS_DIR"
 fi
-
-###########################################################################
-# INSTALL CAKE
-###########################################################################
-
-if [ ! -f "$CAKE_EXE" ]; then
-    mono "$NUGET_EXE" install Cake -Version $CAKE_VERSION -OutputDirectory "$TOOLS_DIR"
-    if [ $? -ne 0 ]; then
-        echo "An error occurred while installing Cake."
-        exit 1
-    fi
-fi
-
-# Make sure that Cake has been installed.
-if [ ! -f "$CAKE_EXE" ]; then
-    echo "Could not find Cake.exe at '$CAKE_EXE'."
-    exit 1
-fi
-
-###########################################################################
-# RUN BUILD SCRIPT
-###########################################################################
 
 export MONO_ROOT=$(dirname $(which mono))/../
 
-# Start Cake
-exec mono "$CAKE_EXE" build.cake --paths_tools=cake ${SCRIPT_ARGUMENTS[@]}
+exec "$CAKE_TOOL" --paths_tools=cake $@
